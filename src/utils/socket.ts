9b4957ba3ae70fc629927612c05140f7430d57fd
Sandeep Kumar <sandeepmahendran@gmail.com>
2023-02-04 21:25:07 +0530

    Changed from query to header

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -38,7 +38,7 @@ export async function initializeSocketServer(app: httpServer): Promise<void> {
     io.use(socketAuth);
     logger.info("Socket server attached!");
     io.on("connection", (socket: SocketInstance) => {
-      const roomId = socket.handshake.query.room;
+      const roomId = socket.handshake.headers["roomId"];
       if (!roomId) {
         socket.disconnect();
       } else {
